18ff752a7e8e48dc09c90b2b991304c031d685e0
xian1022 <c112181113@nkust.edu.tw>
2025-10-30 11:34:52 +0800

    Third upload

---
+++ ch7-2-2 copy.php
@@ -0,0 +1,13 @@
+<?php
+$weekday = array(
+   1 => "Mon",
+        "Thu",
+        "Wed",
+        "Thu", 
+        "Sat",
+        "Sun"
+);
+for ($i = 1; $i <= count(value: $weekday); $i++){
+   echo " $i : $weekday[$i]";
+}
+?>
